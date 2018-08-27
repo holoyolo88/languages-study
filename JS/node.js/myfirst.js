f9415fbf036edbabe284ce098ce622584696f46d
holoyolo88 <ansoh4025@naver.com>
2018-08-27 13:29:11 +0900

    Read the Query String

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -4,6 +4,7 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 // createServer() : create an HTTP server that listens to server ports and gives a response back to the client.
 // : will be executed when someone tries to access the computer on port 8080
+// function : req argument represents the request from the client, as an object(http.IncomingMessage object)
 http.createServer(function (req, res) {
     // writeHead() : if the response from the HTTP server is supposed to be displayed as HTML
     // : include HTTP header with the correct content type
@@ -11,7 +12,8 @@ http.createServer(function (req, res) {
     // : second argument is an object containing the response headers
     res.writeHead(200, {'Content-Type': 'text/html'});
     // write() : write a response to the client
-    res.write("The date and time are currently: "+dt.myDateTime());
+    // url : object's property which holds the part of the url that comes after the domain name
+    res.write(req.url);
     // end() : end the response
     res.end();
 }).listen(8080);
