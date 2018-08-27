3c02d11f04f94f43cd72c6ac7643286b59fa6e4c
holoyolo88 <ansoh4025@naver.com>
2018-08-27 22:24:45 +0900

    Update Files -append

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -1,14 +1,11 @@
-// require() : include a module
-var http = require('http');
 // built-in file system module : allow to work with the file system on computer
 var fs = require('fs');
 // createServer() : create an HTTP server that listens to server ports and gives a response back to the client.
 // : will be executed when someone tries to access the computer on port 8080
 // function : req argument represents the request from the client, as an object(http.IncomingMessage object)
 http.createServer(function (req, res) {
-    // writeFile() : replaces the specified file and content if it exists
-    // : If the file does not exist, a new file, containing the specified content
-    fs.writeFile('file3.html','content',function(err){
+    // appendFile() : appends the specified content at the end of the specified file
+    fs.appendFile('file1.html','content',function(err){
     if(err) throw err;
     console.log('Saved!')  
     });  
