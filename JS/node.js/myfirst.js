8d809375c7b904be3d5ad06d35e5e363afc155e6
holoyolo88 <ansoh4025@naver.com>
2018-08-27 22:15:08 +0900

    Create Files -append

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -6,14 +6,9 @@ var fs = require('fs');
 // : will be executed when someone tries to access the computer on port 8080
 // function : req argument represents the request from the client, as an object(http.IncomingMessage object)
 http.createServer(function (req, res) {
-    // readfile() : read fimes on computer
-    fs.readFile('file.html',function(err,data){
-    // writeHead() : if the response from the HTTP server is supposed to be displayed as HTML
-    // : include HTTP header with the correct content type
-    // : first argument is the status code
-    // : second argument is an object containing the response headers
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();   
+    // appendfile() : if the file does not exist, create a new file
+    fs.appendFile('file1.html','content',function(err){
+    if(err) throw err;
+    console.log('Saved!')  
     });  
 }).listen(8080);
