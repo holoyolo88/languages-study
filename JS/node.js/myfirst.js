58cbf864c1da165a2da8424e61382177887d8093
holoyolo88 <ansoh4025@naver.com>
2018-08-27 22:07:42 +0900

    Read Files

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -1,19 +1,19 @@
 // require() : include a module
 var http = require('http');
-// built-in url module : split the query string into readable parts
-var url = require('url');
+// built-in file system module : allow to work with the file system on computer
+var fs = require('fs');
 // createServer() : create an HTTP server that listens to server ports and gives a response back to the client.
 // : will be executed when someone tries to access the computer on port 8080
 // function : req argument represents the request from the client, as an object(http.IncomingMessage object)
 http.createServer(function (req, res) {
+    // readfile() : read fimes on computer
+    fs.readFile('file.html',function(err,data){
     // writeHead() : if the response from the HTTP server is supposed to be displayed as HTML
     // : include HTTP header with the correct content type
     // : first argument is the status code
     // : second argument is an object containing the response headers
     res.writeHead(200, {'Content-Type': 'text/html'});
-    //  parse() : split the query string into readable parts
-    var q = url.parse(req.url,true).query;
-    var txt = q.year+" "+q.month;
-    // end() : end the response
-    res.end(txt);
+    res.write(data);
+    res.end();   
+    });  
 }).listen(8080);
