142c9773a3e85378f878f7d09be0e12588204fd3
holoyolo88 <ansoh4025@naver.com>
2018-08-28 10:19:59 +0900

    Node.js NPM

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -1,16 +1,19 @@
+// NPM : package manager for Node.js packages or modules
+// : creates a folder named "node_modules", where the package will be placed
+// : all installed package will be in this folder
+// Package : contains all the files needed for a module
+// modules : JS libraries that can included in project
+
 // require() : include a module
 var http = require('http');
-// built-in url module : split the query string into readable parts
-var url = require('url')
-var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-var q = url.parse(adr, true);
-//returns 'localhost:8080'
-console.log(q.host); 
-//returns '/default.htm'
-console.log(q.pathname); 
-//returns '?year=2017&month=february'
-console.log(q.search); 
-//returns an object: { year: 2017, month: 'february' }
-var qdata = q.query;
-//returns 'february'
-console.log(qdata.month); 
+// Upper-Case module : convert output into upper-case letters
+var uc = require('upper-case');
+// createServer() : create an HTTP server that listens to server ports and gives a response back to the client.
+// : will be executed when someone tries to access the computer on port 8080
+// function : req argument represents the request from the client, as an object(http.IncomingMessage object)
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(uc("Hello World!"));
+    res.end();
+}).listen(8080);
+
