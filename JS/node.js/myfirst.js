4c94eabab0cb0f02009ebd98bfb874bd0a9d99a2
holoyolo88 <ansoh4025@naver.com>
2018-08-28 13:13:15 +0900

    The EventEmitter Object

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -1,8 +1,13 @@
-// built-in file system module : allow to work with the file system on computer
-var fs = require('fs');
-// Every action on a computer is an event
-// Objects in Node.js can fire events
-var rs = fs.createReadStream('./file.txt');
-rs.on('open',function() {
-    console.log('The file is open');
-})
+// bulilt-in Events module : create-, fire-, and listen for- events
+var events = require('events');
+// EventEmitter :  all event properties and methods are an instance of an EventEmitter object
+// : access these properties and methods
+var eventEmitter = new events.EventEmitter();
+// Create an event handler
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+}
+// on() : assign the event handler to an event
+eventEmitter.on('scream', myEventHandler);
+//emit() : fire the 'scream' event
+eventEmitter.emit('scream');
