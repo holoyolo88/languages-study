9fb7fee2b9e145988a1d407d42f72821ec2115d8
holoyolo88 <ansoh4025@naver.com>
2018-08-28 10:32:56 +0900

    Events in Node.js

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -1,19 +1,8 @@
-// NPM : package manager for Node.js packages or modules
-// : creates a folder named "node_modules", where the package will be placed
-// : all installed package will be in this folder
-// Package : contains all the files needed for a module
-// modules : JS libraries that can included in project
-
-// require() : include a module
-var http = require('http');
-// Upper-Case module : convert output into upper-case letters
-var uc = require('upper-case');
-// createServer() : create an HTTP server that listens to server ports and gives a response back to the client.
-// : will be executed when someone tries to access the computer on port 8080
-// function : req argument represents the request from the client, as an object(http.IncomingMessage object)
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(uc("Hello World!"));
-    res.end();
-}).listen(8080);
-
+// built-in file system module : allow to work with the file system on computer
+var fs = require('fs');
+// Every action on a computer is an event
+// Objects in Node.js can fire events
+var rs = fs.createReadStream('./file.txt');
+rs.on('open',function() {
+    console.log('The file is open');
+})
