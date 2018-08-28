0457801aef09c9d66269d25f948b064c3a6b3005
holoyolo88 <ansoh4025@naver.com>
2018-08-28 13:26:27 +0900

    Send HTML

--- JS/node.js/myfirst.js
+++ JS/node.js/myfirst.js
@@ -12,7 +12,7 @@ var mailOptions = {
   from: 'youremail@gmail.com',
   to: 'myfriend@yahoo.com, myotherfriend@yahoo.com',
   subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  html: '<h1>Welcome</h1><p>That was easy!</p>'
 };
 // sendMail() :sendMail
 transporter.sendMail(mailOptions, function(error, info){
